4322b218019ec710146d942c2be541087975aecf
stopash <72157760+stopash@users.noreply.github.com>
2023-02-27 01:46:18 -0500

    Rename files, add scheduler state diagram

---
+++ bin/CommandData.java
@@ -0,0 +1,84 @@
+import java.time.LocalTime;
+
+/**
+ * Structure for passing commands between threads
+ * @author Ashwin Stoparczyk
+ *
+ */
+public class CommandData {
+	private LocalTime time;
+	private int startFloor;
+	private int destFloor;
+	private String dir;
+	private String source;
+	private String dest;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param time Time command was made
+	 * @param startFloor Starting floor of elevator
+	 * @param destFloor Destination floor of elevator
+	 * @param dir Direction from starting to destination floor (up or down)
+	 */
+	public CommandData(LocalTime time, int startFloor, int destFloor, String dir, String source, String dest){
+		this.setTime(time);
+		this.setStartFloor(startFloor);
+		this.setDestFloor(destFloor);
+		this.setDir(dir);
+		this.setSource(source);
+		this.setDest(dest);
+	}
+
+
+	/**
+	 * Getters and setters for CommandData fields
+	 */
+	public LocalTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalTime time) {
+		this.time = time;
+	}
+
+	public int getStartFloor() {
+		return startFloor;
+	}
+
+	public void setStartFloor(int startFloor) {
+		this.startFloor = startFloor;
+	}
+
+	public int getDestFloor() {
+		return destFloor;
+	}
+
+	public void setDestFloor(int destFloor) {
+		this.destFloor = destFloor;
+	}
+
+	public String getDir() {
+		return dir;
+	}
+
+	public void setDir(String dir) {
+		this.dir = dir;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getDest() {
+		return dest;
+	}
+
+	public void setDest(String dest) {
+		this.dest = dest;
+	}
+}
---
+++ bin/TestElevatorSystem.java
@@ -0,0 +1,64 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the ElevatorSystem class.
+ * @author Henry Lin
+ *
+ */
+
+class TestElevatorSystem {
+
+	/**
+	 * Stall thread for given amount of time. Used to wait
+	 * since threads may take some time to execute.
+	 * @param millis Time in milliseconds
+	 */
+	private void delayFor(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	/**
+	 * Tests that communications are being received.
+	 * TODO: In Iteration 1, the program terminates after one round of messages.
+	 * Future iterations may change this behavior, so this test should be changed
+	 * too.
+	 */
+	void testCommunicationsReceived() {
+		//List of all pending commands
+		ElevatorCommands commands = new ElevatorCommands();
+		
+		//Create Floor, Elevator, and Scheduler threads
+		Thread floor1 = new Floor(commands);
+		Thread scheduler = new Scheduler(commands);
+		Thread elevator1 = new Elevator(commands);
+		
+		assertTrue(commands.getElevatorSize() == 0);
+		assertTrue(commands.getFloorSize() == 0);
+		assertTrue(commands.getReturnESize() == 0);
+		assertTrue(commands.getReturnFSize() == 0);
+		
+		floor1.start();
+		delayFor(1000);
+		assertTrue(commands.getFloorSize() > 0);
+		// Floor created a command
+		
+		scheduler.start();
+		delayFor(1000);
+		assertTrue(commands.getFloorSize() == 0);
+		assertTrue(commands.getElevatorSize() > 0);
+		// Scheduler consumed the command
+		
+		elevator1.start();
+		// Program should terminate after this, if the elevator received the message.
+		
+		delayFor(10000);
+		fail("Timeout: Floor did not terminate program");
+	}
+
+}
