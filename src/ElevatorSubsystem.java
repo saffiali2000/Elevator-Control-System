c6705c32dfe8f89d6c460b579f29268243e92644
Henry <henrylin4@cmail.carleton.ca>
2023-03-09 18:07:27 -0500

    Add elevator subsystem

---
+++ src/ElevatorSubsystem.java
@@ -0,0 +1,109 @@
+
+
+/**
+ * Simulates the subsystems of an elevator.
+ * @author Henry Lin
+ *
+ */
+
+public class ElevatorSubsystem {
+	public enum ElevatorState {Idle, Open, MovingUp, MovingDown};
+	/** The Elevator's current state. */
+	private ElevatorState currentState;
+	/** Tells whether the door is open or not. */
+	private boolean doorOpen;
+	/** The elevator's current floor. */
+	private int curr;
+	/** The elevator's current destination floor. */
+	private int dest;
+
+	/**
+	 * Create a new ElevatorSubsystem in the Idle state with doors closed.
+	 */
+	public ElevatorSubsystem() {
+		currentState = ElevatorState.Idle;
+		doorOpen = false;
+		curr = 1;
+		dest = 1;
+	}
+	
+	/**
+	 * Handle the buttonPressed event.
+	 */
+	public void handleButtonPressed() {
+		if (currentState == ElevatorState.Idle) {
+			currentState = ElevatorState.Open;
+			openDoors();
+		}
+	}
+	
+	/**
+	 * Simulate the activity of opening the doors.
+	 */
+	private void openDoors() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		doorOpen = true;
+	}
+	
+	/**
+	 * Handle the doorClosed event.
+	 */
+	public void handleDoorClosed() {
+		if (currentState == ElevatorState.Open) {
+			doorOpen = false;
+			if (dest > curr) {
+				currentState = ElevatorState.MovingUp;
+				move();
+			} else if (dest < curr) {
+				currentState = ElevatorState.MovingDown;
+				move();
+			} else if (dest == curr) {
+				currentState = ElevatorState.Idle;
+			}
+		}
+	}
+	
+	/**
+	 * Handle the arrived event.
+	 */
+	public void handleArrived() {
+		if (currentState == ElevatorState.MovingUp || currentState == ElevatorState.MovingDown) {
+			currentState = ElevatorState.Open;
+			openDoors();
+		}
+	}
+	
+	/**
+	 * Move the elevator to the destination floor.
+	 */
+	private void move() {
+		try {
+			Thread.sleep((long) (timeToTravel(curr, dest) * 1000));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		curr = dest;
+	}
+	
+	/**
+	 * Provide the required time (in seconds) to travel between the specified floors.
+	 * @param from Floor to start from
+	 * @param to Floor to end at
+	 * @return The time in seconds to travel between the floors
+	 */
+	public static double timeToTravel(int from, int to) {
+		return (to - from) * 3 + 4;
+	}
+	
+	/**
+	 * Return the elevator's current state.
+	 * @return The elevator's current state
+	 */
+	public ElevatorState getState() {
+		return currentState;
+	}
+}
